013cb00fc043e4a453dc515fec56f67823c43ef0
Bartlomiej Kotarski <bkotarski1@gmail.com>
2024-07-07 22:47:53 +0200

    feat: create mqttclient

--- thermo_mqtt_publisher/src/main.cpp
+++ thermo_mqtt_publisher/src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
-#include <PubSubClient.h>
+
 #include "secrets.h"
+
 #include "./thermistor/thermistor.h"
 #include "./local_connection_manager/local_connection_manager.h"
+#include "./mqtt_client/mqtt_client.h"
 
 #define THERMISTOR_PIN A0
 #define SERIES_RESISTOR 10000    // 10kΩ resistor
@@ -13,14 +15,14 @@
 char ssid[] = SECRET_SSID; // network SSID
 char pass[] = SECRET_PASS; // network password
 
-LocalConnectionManager localConnectionManager(ssid, pass);
-PubSubClient mqttClient(localConnectionManager.getWifiClient());
-
-Thermistor thermistor(SERIES_RESISTOR, NOMINAL_RESISTANCE, NOMINAL_TEMPERATURE, B_COEFFICIENT);
-
 const char broker[] = "192.168.55.102";
 int port = 1883;
 const char topic[] = "temperature-topic";
+
+LocalConnectionManager localConnectionManager(ssid, pass);
+MqttClient mqttClient(broker, port, localConnectionManager.getWifiClient());
+
+Thermistor thermistor(SERIES_RESISTOR, NOMINAL_RESISTANCE, NOMINAL_TEMPERATURE, B_COEFFICIENT);
 
 // set interval for sending messages (milliseconds)
 const long interval = 8000;
@@ -28,49 +30,19 @@ unsigned long previousMillis = 0;
 
 int count = 0;
 
-void publishMessage(const char *message);
-
 void setup()
 {
   Serial.begin(9600);
-
   // while (!Serial) {
   //; // wait for serial port to connect. Needed for native USB port only
   // }
 
   localConnectionManager.connect();
-
-  mqttClient.setServer(broker, port);
-
-  if (!mqttClient.connect("ESP32Client"))
-  {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.state());
-    while (1)
-      ;
-  }
-
-  Serial.println("You're connected to the MQTT broker!");
-  Serial.println();
+  mqttClient.connect();
 }
 
 void loop()
 {
-  if (!mqttClient.connected())
-  {
-    Serial.println("Disconnected from MQTT broker. Reconnecting...");
-    if (mqttClient.connect("ESP32Client"))
-    {
-      Serial.println("Reconnected to MQTT broker");
-    }
-    else
-    {
-      Serial.print("Failed to reconnect. Error code = ");
-      Serial.println(mqttClient.state());
-      delay(5000); // Wait 5 seconds before retrying
-    }
-  }
-
   mqttClient.loop(); // Maintain MQTT connection
 
   int rawValue = analogRead(THERMISTOR_PIN);
@@ -82,20 +54,8 @@ void loop()
 
   char tempStr[16];
   dtostrf(celciousTemperature, 6, 2, tempStr);
-  publishMessage(tempStr);
+
+  mqttClient.publish(topic, tempStr);
 
   delay(1000);
-}
-
-void publishMessage(const char *message)
-{
-  if (mqttClient.publish(topic, message, true))
-  {
-    Serial.print("Message published: ");
-    Serial.println(message);
-  }
-  else
-  {
-    Serial.println("Message publish failed");
-  }
 }
