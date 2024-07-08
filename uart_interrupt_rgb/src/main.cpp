3c2bc46da7ccc4c9a98c1bb5b61336d028ee3c20
Bartlomiej Kotarski <bkotarski1@gmail.com>
2024-07-08 22:28:00 +0200

    feat: add logic for diode

--- uart_interrupt_rgb/src/main.cpp
+++ uart_interrupt_rgb/src/main.cpp
@@ -26,6 +26,8 @@ void setup()
     ; // wait for serial port to connect.
   }
 
+  pinMode(LED_BUILTIN, OUTPUT);
+
   pinMode(ERROR_DIODE, OUTPUT);
   pinMode(BLUE_DIODE, OUTPUT);
   pinMode(GREEN_DIODE, OUTPUT);
@@ -42,15 +44,12 @@ void setup()
 
 void loop()
 {
-  analogWrite(BLUE_DIODE, 255);
-  analogWrite(GREEN_DIODE, 255);
-  analogWrite(RED_DIODE, 255);
+  digitalWrite(LED_BUILTIN, HIGH);
 
   delay(1000);
 
-  analogWrite(BLUE_DIODE, 0);
-  analogWrite(GREEN_DIODE, 0);
-  analogWrite(RED_DIODE, 0);
+  digitalWrite(LED_BUILTIN, LOW);
+
   delay(1000);
 
   if (messageReceived)
@@ -82,15 +81,33 @@ void IRAM_ATTR onUartReceive()
     }
   }
 
+  if (receivedMessage.startsWith("RED"))
+  {
+    int value = receivedMessage.substring(4).toInt(); // Parse the value after "RED"
+    analogWrite(RED_DIODE, value);                    // Set RED diode brightness
+  }
+  else if (receivedMessage.startsWith("GREEN"))
+  {
+    int value = receivedMessage.substring(6).toInt(); // Parse the value after "GREEN"
+    analogWrite(GREEN_DIODE, value);                  // Set GREEN diode brightness
+  }
+  else if (receivedMessage.startsWith("BLUE"))
+  {
+    int value = receivedMessage.substring(5).toInt(); // Parse the value after "BLUE"
+    analogWrite(BLUE_DIODE, value);                   // Set BLUE diode brightness
+  }
+  else
+  {
+    // Turn on the LED
+    digitalWrite(ERROR_DIODE, HIGH);
+
+    // Start the timer for 3 seconds (3000000 microseconds)
+    timerWrite(timer, 0);                   // Reset timer counter
+    timerAlarmWrite(timer, 3000000, false); // Set alarm to 3 seconds (non-repeating)
+    timerAlarmEnable(timer);                // Enable the timer alarm
+  }
+
   messageReceived = true;
-
-  // Turn on the LED
-  digitalWrite(ERROR_DIODE, HIGH);
-
-  // Start the timer for 3 seconds (3000000 microseconds)
-  timerWrite(timer, 0);                   // Reset timer counter
-  timerAlarmWrite(timer, 3000000, false); // Set alarm to 3 seconds (non-repeating)
-  timerAlarmEnable(timer);                // Enable the timer alarm
 }
 
 void IRAM_ATTR onTimer()
